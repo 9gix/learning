cff1189344943f4222684188ccb396a142bbf7d8
Eugene <yeo.eugene.oey@gmail.com>
2021-06-17 19:40:41 +0800

    Count Happy Ducks

---
+++ awesome_ponds.sql
@@ -0,0 +1,12 @@
+SELECT ponds.ID as `POND_ID`, count(ducks.ID) as `HAPPY_DUCKS`
+FROM ((ducks
+INNER JOIN species ON ducks.SPECIES_ID = species.ID)
+RIGHT JOIN ponds ON ducks.POND_ID = ponds.ID)
+WHERE 
+CASE
+	WHEN species.TEMP_PREFERENCES = '-' THEN ponds.TEMPERATURE <= species.TEMP_LIMIT
+    WHEN species.TEMP_PREFERENCES = '+' THEN ponds.TEMPERATURE >= species.TEMP_LIMIT
+    ELSE 1
+END
+GROUP BY ponds.ID
+;
